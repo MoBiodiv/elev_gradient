3db1b4a0d2c480bc6a079d216edc88377f6a8abf
Engel <te19xejy@idiv.de>
2018-09-27 12:23:20 +0200

    Run exploratory analysis with univariate metrics on alpha and gamma scales

---
+++ exploratory analysis/univariate gradients.R
@@ -0,0 +1,34 @@
+library(tidyverse)
+library(mobr)
+dat<-as_tibble(read.csv("data/Species_counts.csv"))
+summary(dat)
+str(dat)
+sites<-as_tibble(read.csv("data/sites.csv"))
+plot_attr<-merge(dat[,1:4], sites, by.x ="Site", by.y="Site.Code", all.x = T)
+table(plot_attr$Site==dat$Site)
+
+mob_in<-make_mob_in(comm=select(dat, -c(1:4)),
+                    plot_attr = plot_attr,coord_names = c("E", "N"))
+
+stats<- get_mob_stats(mob_in,group_var = "Site")
+plot(stats)
+
+alphas<-stats$samples_stats
+alphas$Elevation<- plot_attr$Elevation..m.[match(alphas$group, plot_attr$Site)]
+
+alphas %>%
+  filter(abs(value)<1000) %>%
+  ggplot(aes(Elevation, value))+
+    geom_point()+
+    geom_smooth(method="lm")+
+    facet_wrap(.~index,scales = "free")
+
+gammas<-stats$groups_stats  
+gammas$Elevation<- plot_attr$Elevation..m.[match(gammas$group, plot_attr$Site)]
+
+gamma_plot<- gammas %>%
+              filter(abs(value)<1000) %>%
+                ggplot(aes(Elevation, value))+
+                geom_point()+
+                geom_smooth(method="lm")+
+                facet_wrap(.~index,scales = "free")
