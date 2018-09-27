2daec6d10dac9cc528bbc751de13effe188c0318
Engel <te19xejy@idiv.de>
2018-09-27 14:49:13 +0200

    Add plots of univariate metrics over elevation

--- exploratory analysis/univariate gradients.R
+++ exploratory analysis/univariate gradients.R
@@ -17,18 +17,20 @@ alphas<-stats$samples_stats
 alphas$Elevation<- plot_attr$Elevation..m.[match(alphas$group, plot_attr$Site)]
 
 alphas %>%
+  filter(abs(value)<1000) %>% #some of the S_PIE blow up because of low numbers of individuals
+  ggplot(aes(Elevation, value))+
+    geom_point()+
+    geom_smooth(method="lm")+
+    facet_wrap(.~index,scales = "free")
+ggsave("alphas.pdf",path = "./exploratory analysis")
+
+gammas<-stats$groups_stats  
+gammas$Elevation<- plot_attr$Elevation..m.[match(gammas$group, plot_attr$Site)]
+
+gammas %>%
   filter(abs(value)<1000) %>%
   ggplot(aes(Elevation, value))+
     geom_point()+
     geom_smooth(method="lm")+
     facet_wrap(.~index,scales = "free")
-
-gammas<-stats$groups_stats  
-gammas$Elevation<- plot_attr$Elevation..m.[match(gammas$group, plot_attr$Site)]
-
-gamma_plot<- gammas %>%
-              filter(abs(value)<1000) %>%
-                ggplot(aes(Elevation, value))+
-                geom_point()+
-                geom_smooth(method="lm")+
-                facet_wrap(.~index,scales = "free")
+ggsave("gammas.pdf",path = "./exploratory analysis")
