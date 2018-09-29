f03474fd7b2c7047c7244a6b56df26ec5d3f8d96
Dan McGlinn <danmcglinn@gmail.com>
2018-09-28 20:53:27 -0400

    Add continous analysis, data object, and graphical output

--- scripts/univariate_gradients.R
+++ scripts/univariate_gradients.R
@@ -1,35 +1,19 @@
-library(tidyverse)
 library(mobr)
 
 # read in data files. 
-dat <- as_tibble(read.csv("data/Species_counts.csv"))
-summary(dat)
-str(dat)
-dat$Site <- as.character(dat$Site)
-dat$Site <- ifelse(dat$Site == "CATALOOCHEE", "CATA",
-                   dat$Site)
+ant_comm <- read.csv("data/comm_dat.csv", row.names = 1)
+ant_plot_attr <- read.csv("data/site_dat.csv", row.names = 1)
 
-sites <- as_tibble(read.csv("data/sites.csv"))
-plot_attr <- merge(dat[ , 1:4], sites, by.x = "Site",
-                   by.y = "Site.Code", all.x = T)
+# slightly change elev of RAMY b/c of delta_stats issue
+ant_plot_attr$Elevation_m[ant_plot_attr$Site == 'RAMY'] = 942
 
+ant_mob_in <- make_mob_in(ant_comm, ant_plot_attr)
 
-all(plot_attr$Site == dat$Site)
-
-# drop site id information
-dat <- dat[ , -c(1:4)]
-
-# use UTM coordinates so that Euclidean distances
-# can be sued. 
-mob_in <- make_mob_in(comm = dat[ , -c(1:4)],
-                      plot_attr = plot_attr,
-                      coord_names = c("UTM.E", "UTM.N"))
-
-stats <- get_mob_stats(mob_in, group_var = "Site")
+stats <- get_mob_stats(ant_mob_in, group_var = "Site")
 plot(stats)
 
 alphas <- stats$samples_stats
-alphas$Elevation <- plot_attr$Elevation..m.[match(alphas$group, plot_attr$Site)]
+alphas$Elevation <- ant_plot_attr$Elevation_m[match(alphas$group, plot_attr$Site)]
 
 alphas %>%
   filter(abs(value) < 1000) %>% #some of the S_PIE blow up because of low numbers of individuals
@@ -40,7 +24,7 @@ alphas %>%
 ggsave("alphas.pdf", path = "./figs")
 
 gammas <- stats$groups_stats  
-gammas$Elevation <- plot_attr$Elevation..m.[match(gammas$group, plot_attr$Site)]
+gammas$Elevation <- ant_plot_attr$Elevation_m[match(gammas$group, plot_attr$Site)]
 
 gammas %>%
   filter(abs(value) < 1000) %>%
@@ -49,3 +33,14 @@ gammas %>%
     geom_smooth(method="lm") +
     facet_wrap( . ~ index,scales = "free")
 ggsave("gammas.pdf", path = "./figs")
+
+## continuous analysis
+
+deltas = get_delta_stats(ant_mob_in, group_var = 'Elevation_m',
+                         type = 'continuous', n_perm = 999)    
+
+save(deltas, file = './results/deltas.Rdata')
+
+pdf('./figs/deltas_b1.pdf')
+plot(deltas, stat = 'b1')
+dev.off()
