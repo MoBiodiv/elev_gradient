078b89be23b10308face17b1859c2c44fc66363a
Dan McGlinn <danmcglinn@gmail.com>
2018-10-12 11:20:44 -0400

    Add analysis of composition along elevation gradient

--- scripts/univariate_gradients.R
+++ scripts/univariate_gradients.R
@@ -1,4 +1,5 @@
 library(mobr)
+library(vegan)
 
 # read in data files. 
 ant_comm <- read.csv("data/comm_dat.csv", row.names = 1)
@@ -68,4 +69,28 @@ save(deltas_noNODI, file = './results/deltas_noNODI.Rdata')
 
 pdf('./figs/deltas_noNODI_b1.pdf')
 plot(deltas_noNODI, stat = 'b1')
+dev.off()
+
+# analysis of composition change
+pdf('./figs/rda_results.pdf')
+ant_rda = rda(sqrt(ant_comm) ~ Elevation_m, data = ant_plot_attr)
+ant_rda
+RsquareAdj(ant_rda)
+anova(ant_rda, by = 'terms', permutations = 2e3)
+plot(ant_rda, display = c('sp', 'bp'))
+ant_mso = mso(ant_rda, ant_plot_attr[ , c('UTM_E'< 'UTM_N')], grain = 5000, 
+              permutations = 1000)
+msoplot(ant_mso)
+dev.off()
+
+pdf('./figs/cca_results.pdf')
+elev = ant_plot_attr$Elevation_m[rowSums(ant_comm) > 0]
+xy = ant_plot_attr[rowSums(ant_comm) > 0, c('UTM_E', 'UTM_N')]
+ants = ant_comm[rowSums(ant_comm) > 0, ]
+ant_cca = cca(sqrt(ants) ~ elev)
+RsquareAdj(ant_cca)
+anova(ant_cca, by = 'terms', permutations = 2e3)
+plot(ant_cca, display = c('sp', 'bp'))
+ant_mso_cca = mso(ant_cca, xy, grain = 5000, permutations = 1000)
+msoplot(ant_mso_cca)
 dev.off()
