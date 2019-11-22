faeb61297d5fa3c051a0ee6e587cdc9a088b8bdc
Dan McGlinn <danmcglinn@gmail.com>
2019-11-21 20:24:45 -0500

    Improve cartoon analysis

--- scripts/cartoon_analysis.R
+++ scripts/cartoon_analysis.R
@@ -1,4 +1,7 @@
 library(mobr)
+library(purrr)
+library(dplyr)
+library(ggplot2)
 
 dat = read.csv('./data/cartoon_data.csv')
 
@@ -13,9 +16,31 @@ mob_in_list$sad = subset(mob_in_list$sad, cat == "sad")
 mob_in_list$N = subset(mob_in_list$N, cat == "N")
 mob_in_list$agg = subset(mob_in_list$agg, cat == "agg")
 
-mob = map(mob_in_list, function(x)
+mob = purrr::map(mob_in_list, function(x)
     get_delta_stats(x, 'elev', n_perm = 20))
 
 plot(mob$sad, eff_sub_effort = F, eff_disp_pts = F, eff_disp_smooth = T)
 plot(mob$N, eff_sub_effort = F, eff_disp_pts = F, eff_disp_smooth = T)
 plot(mob$agg, eff_sub_effort = F, eff_disp_pts = F, eff_disp_smooth = T)
+
+
+mob$sad$S_df %>% subset(group == 3) %>%
+   mutate(effort = ifelse(sample == "plot",
+                          effort * 5.5, effort)) %>%
+   group_by(test) %>%
+   ggplot(aes(effort, S)) + 
+    geom_line(aes(group = test, color = test, lty=test))
+
+mob$N$S_df %>% subset(group == 3) %>%
+   mutate(effort = ifelse(sample == "plot",
+                          effort * 5.5, effort)) %>%
+   group_by(test) %>%
+   ggplot(aes(effort, S)) + 
+    geom_line(aes(group = test, color = test, lty=test))
+
+mob$agg$S_df %>% subset(group == 3) %>%
+   mutate(effort = ifelse(sample == "plot",
+                          effort * 5.5, effort)) %>%
+   group_by(test) %>%
+   ggplot(aes(effort, S)) + 
+    geom_line(aes(group = test, color = test, lty=test))
